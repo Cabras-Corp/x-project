a904a49660631bc61ca857d6a667d5be5de04cc5
andreLake <andre.lake@hotmail.com>
2024-07-25 14:26:22 -0300

    branch: upload dev branch

--- xproject/src/main/java/com/cabras_corp/xproject/XprojectApplication.java
+++ xproject/src/main/java/com/cabras_corp/xproject/XprojectApplication.java
@@ -8,6 +8,6 @@ public class XprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XprojectApplication.class, args);
+		System.out.println("Xproject Application Started");
 	}
-
 }
